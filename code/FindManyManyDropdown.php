b49fed36a40cbfc74c4ecd434fbdb67e726edc61
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:42:46 +1300

    Converted to PSR-2

--- code/FindManyManyDropdown.php
+++ code/FindManyManyDropdown.php
@@ -6,74 +6,74 @@
  * this saves people trying to search as they can clearly see items that can be added
  */
 
-class FindManyManyDropdown implements GridField_HTMLProvider, GridField_ActionProvider, GridField_DataManipulator {
-		
+class FindManyManyDropdown implements GridField_HTMLProvider, GridField_ActionProvider, GridField_DataManipulator
+{
+        
 
-	public function getHTMLFragments($gridField) {	
-		
-		Requirements::css(FindManyManyDropdown_PATH . '/css/FindManyManyDropdown.css');
-		Requirements::javascript(FindManyManyDropdown_PATH . '/javascript/FindManyManyDropdownForm.js');
-		
-		$targetFragment = 'before';
-		
-		if ( $gridField->getConfig()->getComponentByType('GridFieldButtonRow') )
-		{
-			$targetFragment = 'buttons-before-right';
-		}
-		
-		$dataClass = ($gridField->list->dataClass); 
-		
-		 $dropdownOptions = new DropdownField(
+    public function getHTMLFragments($gridField)
+    {
+        Requirements::css(FindManyManyDropdown_PATH . '/css/FindManyManyDropdown.css');
+        Requirements::javascript(FindManyManyDropdown_PATH . '/javascript/FindManyManyDropdownForm.js');
+        
+        $targetFragment = 'before';
+        
+        if ($gridField->getConfig()->getComponentByType('GridFieldButtonRow')) {
+            $targetFragment = 'buttons-before-right';
+        }
+        
+        $dataClass = ($gridField->list->dataClass);
+        
+        $dropdownOptions = new DropdownField(
             'gridfield_relationdropdown',
             'Please choose an object',
             Dataobject::get($dataClass)->map("ID", "Title")
         );
         $dropdownOptions->setEmptyString('Select:');
-		
-		$addAction = new GridField_FormAction($gridField, 'gridfield_relationadd',
-			_t('GridField.LinkExisting', "Link Existing"), 'addDDto', 'addDDto');
-		$addAction->setAttribute('data-icon', 'chain--plus');
+        
+        $addAction = new GridField_FormAction($gridField, 'gridfield_relationadd',
+            _t('GridField.LinkExisting', "Link Existing"), 'addDDto', 'addDDto');
+        $addAction->setAttribute('data-icon', 'chain--plus');
 
-		
-		$forTemplate = new ArrayData(array());
-		$forTemplate->Fields = new ArrayList();
-		
-		$forTemplate->Fields->push($dropdownOptions);
-		$forTemplate->Fields->push($addAction);
+        
+        $forTemplate = new ArrayData(array());
+        $forTemplate->Fields = new ArrayList();
+        
+        $forTemplate->Fields->push($dropdownOptions);
+        $forTemplate->Fields->push($addAction);
 
-		return array(
-			$targetFragment => $forTemplate->renderWith('FindManyManyDropdownForm')
-		);
-	}
-	
-	
-	public function getActions($gridField) {
-		return array('addDDto');
-	}
-	
-	public function handleAction(GridField $gridField, $actionName, $arguments, $data) {
+        return array(
+            $targetFragment => $forTemplate->renderWith('FindManyManyDropdownForm')
+        );
+    }
+    
+    
+    public function getActions($gridField)
+    {
+        return array('addDDto');
+    }
+    
+    public function handleAction(GridField $gridField, $actionName, $arguments, $data)
+    {
+        if (isset($data['gridfield_relationdropdown']) && $data['gridfield_relationdropdown']) {
+            $gridField->State->GridFieldAddRelation = $data['gridfield_relationdropdown'];
+        }
+        $gridField->State->GridFieldSearchRelation = '';
+    }
 
-		if(isset($data['gridfield_relationdropdown']) && $data['gridfield_relationdropdown']){
-			$gridField->State->GridFieldAddRelation = $data['gridfield_relationdropdown'];
-		}
-		$gridField->State->GridFieldSearchRelation = '';
-			
-	}
-
-	
-	public function getManipulatedData(GridField $gridField, SS_List $dataList) {
-		if(!$gridField->State->GridFieldAddRelation) {
-			return $dataList;
-		}
-		$objectID = Convert::raw2sql($gridField->State->GridFieldAddRelation);
-		if($objectID) {
-			$object = DataObject::get_by_id($dataList->dataclass(), $objectID);
-			if($object) {
-				$dataList->add($object);
-			}
-		}
-		$gridField->State->GridFieldAddRelation = null;
-		return $dataList;
-	}	
-	
+    
+    public function getManipulatedData(GridField $gridField, SS_List $dataList)
+    {
+        if (!$gridField->State->GridFieldAddRelation) {
+            return $dataList;
+        }
+        $objectID = Convert::raw2sql($gridField->State->GridFieldAddRelation);
+        if ($objectID) {
+            $object = DataObject::get_by_id($dataList->dataclass(), $objectID);
+            if ($object) {
+                $dataList->add($object);
+            }
+        }
+        $gridField->State->GridFieldAddRelation = null;
+        return $dataList;
+    }
 }
